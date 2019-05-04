a80dd303038fa0c59236399ab327531e0e1f4814
unknown <sethi.nishant43@gmail.com>
2019-05-04 22:46:57 +0530

    completed the blockchain

--- blockchain.py
+++ blockchain.py
@@ -21,9 +21,9 @@ class Blockchain:
         def proof_of_work(self, previous_proof):
             new_proof = 1
             check_proof = False
-            while check_proof = False:
+            while check_proof is False:
                 hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-                if hash_operation[:4] = '0000':
+                if hash_operation[:4] == '0000':
                     check_proof = True
                 else:
                     new_proof +=1
@@ -32,3 +32,19 @@ class Blockchain:
         def hash(self, block):
             encoded_block = json.dumps(block, sort_keys = True).encode()
             return hashlib.sha256(encoded_block).hexdigest()
+
+        def is_chain_valid(self, chain):
+            previous_block = chain[0]
+            block_index = 1
+            while block_index < len(chain):
+                block = chain[block_index]
+                if block['previous_hash'] != self.hash(previous_block):
+                    return False
+                previous_proof = previous_block['proof']
+                proof = block['proof']
+                hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+                if hash_operation[:4] != '0000':
+                    return False
+                previous_block = block
+                block_index +=1
+            return True
